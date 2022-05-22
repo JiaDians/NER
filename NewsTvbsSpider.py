a5cde14936a982a4bd8c82a6bca099cd0ec32d17
unknown <legendspy1212345@gmail.com>
2022-05-22 14:55:50 +0800

    Update D.txt to 92 records

--- NewsTvbsSpider.py
+++ NewsTvbsSpider.py
@@ -2,16 +2,19 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-url = 'https://news.tvbs.com.tw/world'
-web = requests.get(url)
-soup = BeautifulSoup(web.text, 'html.parser')
-news_now2_div_tag = soup.find('div',class_='news_now2')
-soup2 = BeautifulSoup(str(news_now2_div_tag), 'html.parser')
-a_tags = soup2.findAll('a')
+url1 = 'https://news.tvbs.com.tw/world' # 全球
+url2 = 'https://news.tvbs.com.tw/local' # 社會
+
 
 def GetData(date):
-    with open('A.txt', 'w') as f:
-        for i in range(3):
+    web = requests.get(url2)
+    soup = BeautifulSoup(web.text, 'html.parser')
+    news_now2_div_tag = soup.find('div',class_='news_now2')
+    soup2 = BeautifulSoup(str(news_now2_div_tag), 'html.parser')
+    a_tags = soup2.findAll('a')
+    
+    with open('A.txt', 'a') as f:
+        for i in range(8):
             print('https://news.tvbs.com.tw' + a_tags[i].get('href'))
             web2 = requests.get('https://news.tvbs.com.tw' + a_tags[i].get('href'))
             soup3 = BeautifulSoup(web2.text, 'html.parser')
@@ -20,7 +23,9 @@ def GetData(date):
             date = re.search('發佈時間：(.+?) ', str(author_div_tag))
             data1_dict = dict()
             data1_dict['date'] = date.group(1).strip()
-            if date != data1_dict['date']:
+            # print(data1_dict['date'], date.group(1))
+            if date.group(1) != data1_dict['date']:
+                print('this article is wrong date')
                 continue
             # article
             article_div_tag = soup3.find('div',class_='article_content')
@@ -31,7 +36,7 @@ def GetData(date):
             f.write(str(data1_dict) + '\n')
         
 if __name__ == '__main__':
-    GetData('2022/05/19')
+    GetData('2022/05/22')
         
 
 
